f150e92b2d06fc92c3512d28e8bedc85451455a8
Eric Chen <eric@tron.network>
2020-01-02 13:22:07 -0800

    Enable migrations for host and renter settings
    1) If upgrading from v0, enable renter by default.
    2) If upgrading from v0, enable host if analytics = true (opted in).

--- migrations.go
+++ migrations.go
@@ -23,11 +23,28 @@ func migrate_2_StatusUrl(cfg *Config) bool {
 	return false
 }
 
+func migrate_3_StorageSettings(cfg *Config, fromV0 bool) bool {
+	// 1) Enable host if user opted in (analytics = true) AND
+	// it is a new upgrade from 0.x.x version
+	// 2) Enable renter if it is a new upgrade from 0.x.x version
+	if !fromV0 {
+		return false
+	}
+	// no error possible
+	Profiles["storage-client"].Transform(cfg)
+	if cfg.Experimental.Analytics {
+		Profiles["storage-host"].Transform(cfg)
+	}
+	return true
+}
+
 // MigrateConfig migrates config options to the latest known version
 // It may correct incompatible configs as well
 func MigrateConfig(cfg *Config) bool {
 	updated := false
-	updated = migrate_1_Services(cfg) || updated
+	upToV1 := migrate_1_Services(cfg)
+	updated = upToV1 || updated
 	updated = migrate_2_StatusUrl(cfg) || updated
+	updated = migrate_3_StorageSettings(cfg, upToV1) || updated
 	return updated
 }
