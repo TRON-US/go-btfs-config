a78f7ee6c609e311a81b1a709c71d76d3a83c13e
steve <stevzhang01@gmail.com>
2022-03-21 18:28:14 +0800

    fix: eliminate IPFS and IPNS filed in the config file

--- mounts.go
+++ mounts.go
@@ -2,7 +2,10 @@ package config
 
 // Mounts stores the (string) mount points
 type Mounts struct {
-	IPFS           string
-	IPNS           string
+	IPFS           string `json:"IPFS,omitempty"`
+	IPNS           string `json:"IPNS,omitempty"`
 	FuseAllowOther bool
+
+	BTFS string
+	BTNS string
 }
